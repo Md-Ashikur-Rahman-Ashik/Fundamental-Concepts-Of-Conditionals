ccd9b29284297fade7a074abd90879b3851dfbb6
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-01-26 23:45:11 +0600

    Learning JS conditionals

---
+++ variable.js
@@ -0,0 +1,51 @@
+var sunglassPrice = 80;
+var subject = "EEE";
+var passed = true;
+
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log( 5 == 10);
+// console.log( 10 == 10);
+// console.log( 10 >= 10);
+// console.log( 15 >= 10);
+
+var guavaPrice = 100;
+var applePrice = 250;
+
+// console.log(guavaPrice > applePrice);
+
+// if(3 < 10){
+//     console.log("He is furious");
+// }
+
+var biryaniPrice = 500;
+
+if(biryaniPrice < 300){
+    console.log("Uncle, please give me some biryani.");
+}
+
+var weight = 40;
+if(weight < 20){
+    console.log("I will carry it");
+}
+// else{
+//     console.log("I will rent a car");
+// }
+
+const salary = 25000;
+const isBCS = true;
+const height = 61;
+
+// if(salary > 20000 && height > 66){
+//     console.log("Perfect choice");
+// }
+// else{
+//     console.log("Another selection");
+// }
+
+if(salary > 25000 || height > 72){
+    console.log("Done")
+}
+else{
+    console.log("Another selection");
+}
