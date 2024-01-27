7176cceba3c689a90dffec7c3c87686d9eabfff1
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-01-27 11:58:38 +0600

    Learning nested conditions

---
+++ grade.js
@@ -0,0 +1,30 @@
+const sscMark = 20;
+
+if(sscMark >= 80){
+    console.log("Congrats. You have got A+");
+}
+else if(sscMark >= 70){
+    console.log("Don't worry. You have done good. You have got A");
+}
+else if(sscMark >= 60){
+    console.log("It's fact that you have got A-");
+}
+else if(sscMark >= 50){
+    console.log("You have to work hard. You have got B");
+}
+else if(sscMark >= 40){
+    console.log("It's sad that you have got C");
+}
+else if(sscMark >= 33){
+    console.log("You have passed with a poor mark. You have got D");
+}
+else{
+    console.log("You should never give up and try again next time. Because you didn't pass.");
+}
+
+// We will now learn about nested conditions
+const money = 500;
+
+if(money > 300){
+    console.log("Bro, you are rich");
+}
--- stepped.js
+++ stepped.js
@@ -1,17 +1,46 @@
-const price = 5000;
+// const price = 5000;
 
-if(price >= 5000){
-    const discountAmount = price * 10 / 100;
-    console.log(discountAmount);
-    const payAmount = price - discountAmount;
-    console.log(payAmount);
+// if(price >= 5000){
+//     const discountAmount = price * 10 / 100;
+//     // console.log(discountAmount);
+//     const payAmount = price - discountAmount;
+//     // console.log(payAmount);
+// }
+// else if(price > 2500){
+//     const discountAmount = price * 5 / 100;
+//     // console.log(discountAmount);
+//     const payAmount = price - discountAmount;
+//     // console.log(payAmount);
+// }
+// else{
+//     // console.log(price);
+// }
+
+const age = 44;
+const price = 500;
+if(age <= 12){
+    console.log("You can have free food.");
 }
-else if(price > 2500){
-    const discountAmount = price * 5 / 100;
+else if(age >= 40){
+    console.log("You have got 10% discount. Now you will pay:");
+    const discount = price * 10 / 100;
+    const discountAmount = price - discount;
     console.log(discountAmount);
-    const payAmount = price - discountAmount;
-    console.log(payAmount);
+}
+else if(age >= 50){
+    console.log("You have got 25% discount. Now you will pay:");
+    const discount = price * 25 / 100;
+    const discountAmount = price - discount;
+    console.log(discountAmount);
+}
+else if(age >= 60){
+    console.log("You have got 50% discount. Now you will pay:");
+    const discount = price * 50 / 100;
+    const discountAmount = price - discount;
+    console.log(discountAmount);
 }
 else{
+    console.log("You will have to pay:");
     console.log(price);
+
 }
