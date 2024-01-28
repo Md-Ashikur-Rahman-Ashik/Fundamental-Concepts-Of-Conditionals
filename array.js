45f62c8b76a552bc5450dd130d7d6cecb6799537
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-01-28 12:48:25 +0600

    Learning Array done

---
+++ array.js
@@ -0,0 +1,40 @@
+// const numbers = [1, 2, 3, 4];
+
+const passed = [true, false, true, false];
+
+// const numbers = [1, 5, 66, 11, 45, 65];
+// console.log(numbers.length);
+
+// const numbers = [88, 77, 99, 421, 54, 63];
+// console.log(numbers);
+// console.log(numbers[0]);
+
+// const fourth = numbers[3];
+// console.log(fourth);
+
+let abc = 45;
+abc = 99;
+// numbers[1] = 999;
+// console.log(numbers);
+
+const ages = [];
+const numbers = [12, 87, 98, 45];
+
+// console.log(numbers);
+// numbers.push(44);
+// console.log(numbers);
+
+const friends = [ "Alvi", "Sami", "Mamun", "Sabit"];
+// friends.push("Ashik");
+// console.log(friends);
+
+// friends.pop();
+// console.log(friends);
+// friends.pop();
+// console.log(friends);
+
+friends.shift()
+console.log(friends);
+
+friends.unshift("Ratul");
+console.log(friends);
---
+++ includes.js
@@ -0,0 +1,67 @@
+const friends = [ "Alvi", "Sami", "Mamun", "Sabit"];
+// console.log(friends.includes("Sami"));
+// console.log(friends.includes("Safi"));
+
+// if(friends.includes("Safi")){
+//     console.log("Party");
+// }
+// else{
+//     console.log("We are fasting");
+// }
+
+// console.log(friends.indexOf("Alvi"));
+// console.log(friends.indexOf("Sifat"));
+
+const nums = [];
+// const food = "Kabab Nan Biryani Salad"
+const age = 14;
+
+// console.log(Array.isArray(friends));
+// console.log(Array.isArray(nums));
+// console.log(Array.isArray(food));
+// console.log(Array.isArray(age));
+
+// First task
+// const fruits = ["Banana", "Apple", "Jackfruit", "Carrot", "Guava"];
+// console.log(fruits[2]);
+
+// fruits[1] = "Jambura";
+// console.log(fruits);
+
+// Second task
+// const tour = ["Turkiye", "Afganistan", "Maldives"];
+// tour.push("Indonesia");
+// console.log(tour);
+
+// tour.push("Srilanka", "Kashmir");
+// console.log(tour);
+
+// tour.pop();
+// console.log(tour);
+
+// Third task
+// const books = ["Rich Dad Poor Dad", "Do Epic Shit", "Get Epic Shit Done", "Make Epic Money"];
+// console.log(books.includes("Javascript"));
+// console.log("Javascript book isn't present in the book list");
+
+// Fourth task
+// const salary = [10000, 20000, 30000];
+// const due = "It's Okay, It's in the testing phase.";
+// const roll = 1
+
+// console.log(Array.isArray(salary));
+// console.log("Salary is Array");
+
+// console.log(Array.isArray(due));
+// console.log("Due is not Array");
+
+// console.log(Array.isArray(roll));
+// console.log("Roll is not Array");
+
+// Fifth Task
+const food = ["Milk", "Potato", "Burger", "Olive Oil"];
+const grade = ["Fresh", "Fresh", "Unhealthy", "Fresh"];
+
+console.log("The first array is:", food);
+console.log("The second array is:", grade);
+console.log("The final array is:", food.concat(grade));
